de12011d0afa54cd38590684cda2b2e3fa4893d6
Priya Thorat <160406514+piyathorat@users.noreply.github.com>
2026-02-11 21:31:04 +0530

    Fix home view reviews bug

--- greatkart/views.py
+++ greatkart/views.py
@@ -1,14 +1,14 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from store.models import Product, ReviewRating
-def home(req):
-    products=Product.objects.all().filter(is_available=True).order_by('created_date')
 
-     #get the review rating
-    for product  in products:
-        reviews=ReviewRating.objects.filter(product_id=product.id,status=True)
-    context={
-        'products':products,
-        'reviews':reviews,
+def home(req):
+    products = Product.objects.filter(is_available=True).order_by('created_date')
+
+    reviews = ReviewRating.objects.filter(status=True)
+
+    context = {
+        'products': products,
+        'reviews': reviews,
     }
-    return render(req,'home.html',context)
+
+    return render(req, 'home.html', context)
